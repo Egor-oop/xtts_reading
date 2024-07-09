e4654ab7a092e2f0d18881cac95f7387c2e29e79
Egor-oop <gulidoegor@gmail.com>
2024-07-10 00:05:01 +0300

    synthesizing book

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import re
-
 import mobi
-from bs4 import BeautifulSoup
 import torch
 import torchaudio
+from pysbd.segmenter import Segmenter
+from bs4 import BeautifulSoup
 from TTS.tts.configs.xtts_config import XttsConfig
 from TTS.tts.models.xtts import Xtts
 from TTS.utils.manage import ModelManager
 
+segmenter = Segmenter(clean=True)
 
-def get_chapters(filename: str) -> list[str]:
+
+def get_chapters(filename: str) -> list[list[str]]:
     tempdir, filepath = mobi.extract(filename)
     with open(filepath, 'r') as file:
         content = file.read()
@@ -18,7 +20,6 @@ def get_chapters(filename: str) -> list[str]:
     text = ''
     for tag in tags:
         if tag.name == 'p':
-            print(len(tag.text))
             text += f'{tag.text} '
         elif tag.name == 'br':
             text += '\n '
@@ -26,6 +27,9 @@ def get_chapters(filename: str) -> list[str]:
     text = r.split(text)
     chapter_regex = re.compile(r'Chapter \d+', re.IGNORECASE)
     chapters = chapter_regex.split(text[0])[1:]
+    # chapters = [chapter.split('\n') for chapter in chapters]
+    chapters = [segmenter.segment(chapter) for chapter in chapters]
+    # print(segmenter.segment(chapters[0][1]))
     return chapters
 
 
@@ -45,16 +49,36 @@ def init_model():
     return model
 
 
-def synthesize_chapter(text, chapter_num, gpt_cond_latent, speaker_embedding, model) -> None:
+def merge_wav_files(wav_files, output_path):
+    waveforms = []
+    sample_rate = 0
+    for wav_file in wav_files:
+        waveform, sample_rate = torchaudio.load(wav_file)
+        waveforms.append(waveform)
+    combined_waveform = torch.cat(waveforms, dim=1)
+    torchaudio.save(output_path, combined_waveform, sample_rate)
 
-    out = model.inference(
-        text,
-        'en',
-        gpt_cond_latent,
-        speaker_embedding,
-        temperature=0.7
-    )
-    torchaudio.save(f'Chapter {chapter_num}.wav', torch.tensor(out['wav']).unsqueeze(0), 24000)
+
+def synthesize_chapter(chapter, chapter_num, gpt_cond_latent, speaker_embedding, model) -> None:
+    paragraphs = []
+    for i, sentence in enumerate(chapter):
+        print(len(sentence))
+        out = model.inference(
+            sentence,
+            'en',
+            gpt_cond_latent,
+            speaker_embedding,
+            temperature=0.7,
+            top_k=75,
+            top_p=0.9
+        )
+        wav_filepath = f'audio/temp/c{chapter_num+1}_p{i+1}.wav'
+        torchaudio.save(wav_filepath,
+                        torch.tensor(out['wav']).unsqueeze(0),
+                        24000)
+        paragraphs.append(wav_filepath)
+    res_filepath = f'audio/result/chapter_{chapter_num+1}.wav'
+    merge_wav_files(paragraphs, res_filepath)
     print(f'Finished chapter {chapter_num}')
 
 
@@ -63,57 +87,17 @@ def main() -> None:
     model = init_model()
     print('Computing speaker latents...')
     gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(
-        audio_path=['audio/yegor_en.wav']
+        audio_path=['audio/reference/yegor_en.wav']
     )
-
-    for index in range(len(chapters)):
-        synthesize_chapter(chapters[index],
+    
+    for index, chapter in enumerate(chapters):
+        synthesize_chapter(chapter,
                            index,
                            gpt_cond_latent,
                            speaker_embedding,
                            model)
-
     print('DONE!')
 
 
 if __name__ == '__main__':
     main()
-
-# manager = ModelManager()
-# print('Loading model...')
-# model_name = 'tts_models/multilingual/multi-dataset/xtts_v2'
-# xtts_path, _, _ = manager.download_model(model_name)
-# config = XttsConfig()
-# config.load_json(xtts_path + '/config.json')
-# model = Xtts.init_from_config(config)
-# model.load_checkpoint(config,
-#                       checkpoint_dir=xtts_path,
-#                       use_deepspeed=True if torch.cuda.is_available() else False)
-# if torch.cuda.is_available():
-#     model.cuda()
-#
-# print('Computing speaker latents...')
-# gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(audio_path=['audio/yegor_en.wav'])
-#
-# print('Inference...')
-# print('First')
-# out = model.inference(
-#     'It took me quite a long time to develop a voice and now that I have it I am not going to be silent.',
-#     'en',
-#     gpt_cond_latent,
-#     speaker_embedding,
-#     temperature=0.7
-# )
-# torchaudio.save('output.wav', torch.tensor(out['wav']).unsqueeze(0), 24000)
-#
-# print('Second')
-# out = model.inference(
-#     'It doesn\'t bother me! But what\'s your name?',
-#     'en',
-#     gpt_cond_latent,
-#     speaker_embedding,
-#     temperature=0.7
-# )
-# torchaudio.save('output2.wav', torch.tensor(out['wav']).unsqueeze(0), 24000)
-#
-# print('DONE!')
